ab7d6a2f91add491a84c389116d8fef64ec74b30
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-29 18:02:37 -0500

    working timezone and locale override

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,16 @@
 /// <reference types="cypress" />
+import 'cypress-cdp'
 
-it('shows locale and timezone', () => {
+// this test works really nice in Chrome Canary v110
+// and sometimes fails in Electron v106
+it('shows locale and timezone', { browser: 'Chrome' }, () => {
+  cy.CDP('Emulation.setLocaleOverride', { locale: 'de_DE' })
+  cy.CDP('Emulation.setTimezoneOverride', { timezoneId: 'Europe/Berlin' })
   cy.visit('index.html')
+  cy.contains('locale: de-DE').should('include.text', 'Berlin')
+
+  // clear any current override
+  cy.CDP('Emulation.setTimezoneOverride', { timezoneId: '' })
+  cy.reload()
+  cy.contains('locale: de-DE').should('not.include.text', 'Berlin')
 })
