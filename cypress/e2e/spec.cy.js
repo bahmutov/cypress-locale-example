8efecfe6ff44f65b5f5e4053026f294983bd32d7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-29 21:33:45 -0500

    comments

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,18 @@
 /// <reference types="cypress" />
+// install the cypress-cdp plugin
 
 it('shows locale and timezone', () => {
+  // set the timezone to Berlin
+  // set the locale to "de_DE"
   cy.visit('index.html')
+  // confirm the output element
+  // shows the emulated timezone and locale
+  // https://on.cypress.io/contains
+  //
+  // reset the timezone
+  // and reload the page
+  // https://on.cypress.io/reload
+  // confirm the page still shows the emulated locale
+  // but no longer shows the Berlin timezone
+  // (unless you ARE in Berlin timezone)
 })
